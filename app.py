264e2a65882e5be1956333a73f4b64e201e9ddce
hugofhb <130184468+hugofhb@users.noreply.github.com>
2023-04-13 22:51:25 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import os
-import io
 
 from flask import Flask, request ,render_template
 from tchan import ChannelScraper
@@ -7,10 +6,27 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import requests
 from datetime import date, timedelta
+import telegram
+import pandas as pd
+
 
 TELEGRAM_API_KEY = os.environ["TELEGRAM_API_KEY"]
 bot = telegram.Bot(token=os.environ["TELEGRAM_API_KEY"])
 TELEGRAM_ADMIN_ID = os.environ["TELEGRAM_ADMIN_ID"]
 
-
 app = Flask(__name__)
+
+menu = """ 
+<a href="/">Página inicial</a> | <a href="/sobre">Sobre</a> |
+<br>
+"""
+
+@app.route("/")
+def index():
+    menu = """<a href="/">Página inicial</a> | <a href="/sobre">Sobre</a> |"""
+    projetos = projetos_aprovados()
+    return menu + "Olá, este é o site do robô sobre PLs aprovadas.<br><br>Projetos de Lei Aprovados:<br>" + "<br>".join(projetos)
+
+@app.route("/sobre")
+def sobre():
+    return menu + "Aqui vai o conteúdo da página Sobre"
