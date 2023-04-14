646be2bfd62243e73d9d5c7cf7ed54867555743e
hugofhb <130184468+hugofhb@users.noreply.github.com>
2023-04-13 22:41:08 -0300

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import requests
 from datetime import date, timedelta
-import telegram 
 
 TELEGRAM_API_KEY = os.environ["TELEGRAM_API_KEY"]
 bot = telegram.Bot(token=os.environ["TELEGRAM_API_KEY"])
